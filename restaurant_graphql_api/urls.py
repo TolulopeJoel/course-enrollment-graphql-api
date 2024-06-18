5f93a5b9899779e9d069dad7598edb56c2577697
Tolu Joel <toluisjoel@gmail.com>
2024-06-18 09:07:20 +0100

    chore: Add courses app

--- restaurant_graphql_api/urls.py
+++ restaurant_graphql_api/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("courses.urls")),
     path("", include("accounts.urls")),
     path("", include("restaurant.urls")),
     path("admin/", admin.site.urls),
