6503ef5f51cabc7fa1e7467ea086030914ecbe60
Tolu Joel <toluisjoel@gmail.com>
2024-06-21 07:38:19 +0100

    feat: Add JWT authentication backend and GraphQL JWT configuration

--- user_management_service/core/settings.py
+++ user_management_service/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # local apps
-    "apps.accounts.apps.AccountsConfig"
+    "apps.accounts.apps.AccountsConfig",
+
+    # 3rd party apps
+    "graphene_django",
+    "graphql_jwt.refresh_token",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +129,26 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+GRAPHENE = {
+    'SCHEMA': 'core.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
+}
+
+GRAPHQL_JWT = {
+    "JWT_AUTH_HEADER_PREFIX": "Bearer",
+    "JWT_VERIFY_EXPIRATION": True,
+    "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
+    "JWT_EXPIRATION_DELTA": timedelta(minutes=10),
+    "JWT_REFRESH_EXPIRATION_DELTA": timedelta(days=7),
+    "JWT_SECRET_KEY": SECRET_KEY,
+    "JWT_ALGORITHM": "HS256",
+}
