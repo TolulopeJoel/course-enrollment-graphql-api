b0902a6d2c0c86d223e0f3d1e67888cf1110a7d0
Tolu Joel <toluisjoel@gmail.com>
2024-06-23 03:54:24 +0100

    feat(course): Add mutation to create course

---
+++ course_management_service/utils.py
@@ -0,0 +1,40 @@
+import requests
+
+API_GATEWAY_URL = "http://localhost:4001/graphql"
+
+
+def check_user_exists(token):
+    query = """
+    query {
+        whoami {
+            id
+            email
+        }
+    }
+    """
+    response = requests.post(
+        API_GATEWAY_URL,
+        json={'query': query},
+        headers={'Authorization': token}
+    )
+
+    if response.status_code == 200:
+        return response.json().get('data', {}).get('whoami')
+
+
+def validate_token(token):
+    # Remove 'Bearer ' prefix if present
+    if token.startswith('Bearer '):
+        token = token[7:]
+
+    query = """
+    mutation($token: String!) {
+        verifyToken(token: $token)
+    }
+    """
+    variables = {'token': token}
+    response = requests.post(
+        API_GATEWAY_URL,
+        json={'query': query, 'variables': variables}
+    )
+    return 'payload' in response.json().get('data', {}).get('verifyToken')
